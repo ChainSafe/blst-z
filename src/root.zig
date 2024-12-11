c8ef2fbe22bb4afb38e9df372e70d4dabcd41333
Tuyen Nguyen <vutuyen2636@gmail.com>
2024-12-11 13:11:50 +0700

    feat: implement PublicKey

--- src/root.zig
+++ src/root.zig
@@ -6,16 +6,8 @@ const c = @cImport({
 });
 
 const util = @import("util.zig");
-
-const BLST_ERROR = error{
-    BAD_ENCODING,
-    POINT_NOT_ON_CURVE,
-    POINT_NOT_IN_GROUP,
-    AGGR_TYPE_MISMATCH,
-    VERIFY_FAIL,
-    PK_IS_INFINITY,
-    BAD_SCALAR,
-};
+const BLST_ERROR = util.BLST_ERROR;
+const toBlstError = util.toBlstError;
 
 /// TODO: the size of SecretKey is only 32 bytes so go with stack allocation
 /// consider adding heap allocation with allocator in the future
@@ -82,6 +74,87 @@ const SecretKey = struct {
         var sk = SecretKey.default();
         c.blst_derive_child_eip2333(&sk.value, &self.value, child_index);
         return sk;
+    }
+};
+
+// TODO: implement Clone, Copy, Equal
+const PublicKey = struct {
+    point: c.blst_p1_affine,
+
+    pub fn default() PublicKey {
+        return .{
+            .point = util.default_blst_p1_affline(),
+        };
+    }
+
+    // Core operations
+
+    // key_validate
+    pub fn validate(self: *const PublicKey) BLST_ERROR!void {
+        if (c.blst_p1_affine_is_inf(&self.point) == false) {
+            return BLST_ERROR.PK_IS_INFINITY;
+        }
+
+        if (c.blst_p1_affine_in_g1(&self.point) == false) {
+            return BLST_ERROR.POINT_NOT_IN_GROUP;
+        }
+    }
+
+    pub fn key_validate(key: []const u8) BLST_ERROR!void {
+        const pk = PublicKey.fromBytes(key);
+        return pk.validate();
+    }
+
+    // TODO: from_aggregate
+
+    // Serdes
+
+    pub fn compress(self: *const PublicKey) [48]u8 {
+        var pk_comp = [_]u8{0} ** 48;
+        c.blst_p1_affine_compress(&pk_comp[0], &self.point);
+        return pk_comp;
+    }
+
+    pub fn serialize(self: *const PublicKey) [96]u8 {
+        var pk_out = [_]u8{0} ** 96;
+        c.blst_p1_affine_serialize(&pk_out[0], &self.point);
+        return pk_out;
+    }
+
+    pub fn uncompress(pk_comp: []const u8) BLST_ERROR!PublicKey {
+        if (pk_comp.len == 48 and (pk_comp[0] & 0x80) == 0) {
+            var pk = PublicKey.default();
+            const res = c.blst_p1_uncompress(&pk.point, &pk_comp[0]);
+            const err = toBlstError(res);
+            if (err != null) {
+                return err;
+            }
+            return pk;
+        }
+    }
+
+    pub fn deserialize(pk_in: []const u8) BLST_ERROR!PublicKey {
+        if ((pk_in.len == 96 and (pk_in[0] & 0x80) == 0) or
+            (pk_in.len == 48 and (pk_in[0] & 0x80) != 0))
+        {
+            var pk = PublicKey.default();
+            const res = c.blst_p1_deserialize(&pk.point, &pk_in[0]);
+            const err = toBlstError(res);
+            if (err != null) {
+                return err;
+            }
+            return pk;
+        }
+
+        return BLST_ERROR.BAD_ENCODING;
+    }
+
+    pub fn fromBytes(pk_in: []const u8) BLST_ERROR!PublicKey {
+        return PublicKey.deserialize(pk_in);
+    }
+
+    pub fn toBytes(self: *const PublicKey) [48]u8 {
+        return self.compress();
     }
 };
 
--- src/util.zig
+++ src/util.zig
@@ -2,8 +2,44 @@ const c = @cImport({
     @cInclude("blst.h");
 });
 
+pub const BLST_ERROR = error{
+    BAD_ENCODING,
+    POINT_NOT_ON_CURVE,
+    POINT_NOT_IN_GROUP,
+    AGGR_TYPE_MISMATCH,
+    VERIFY_FAIL,
+    PK_IS_INFINITY,
+    BAD_SCALAR,
+};
+
+pub fn toBlstError(err: c.uint) ?BLST_ERROR {
+    switch (err) {
+        .BLST_BAD_ENCODING => return BLST_ERROR.BAD_ENCODING,
+        .BLST_POINT_NOT_ON_CURVE => return BLST_ERROR.POINT_NOT_ON_CURVE,
+        .BLST_POINT_NOT_IN_GROUP => return BLST_ERROR.POINT_NOT_IN_GROUP,
+        .BLST_AGGR_TYPE_MISMATCH => return BLST_ERROR.AGGR_TYPE_MISMATCH,
+        .BLST_VERIFY_FAIL => return BLST_ERROR.VERIFY_FAIL,
+        .BLST_PK_IS_INFINITY => return BLST_ERROR.PK_IS_INFINITY,
+        .BLST_BAD_SCALAR => return BLST_ERROR.BLST_BAD_SCALAR,
+        else => return null,
+    }
+}
+
 pub fn default_blst_scalar() c.blst_scalar {
     return c.blst_scalar{
         .b = [_]u8{0} ** 32,
+    };
+}
+
+pub fn default_blst_p1_affline() c.blst_p1_affine {
+    return .{
+        .x = default_blst_fp(),
+        .y = default_blst_fp(),
+    };
+}
+
+pub fn default_blst_fp() c.blst_fp {
+    return .{
+        .l = [_]u64{0} ** 6,
     };
 }
