da3e87d8b47f28879a75d1c2ea9fbee50581a1e8
Tuyen Nguyen <twoeths@users.noreply.github.com>
2024-12-12 15:47:50 +0700

    fix: test_aggregate unit test

--- src/root.zig
+++ src/root.zig
@@ -29,37 +29,47 @@ const SecretKey = struct {
         var sk = SecretKey.default();
         const key_info_ptr = if (key_info != null) &key_info.?[0] else null;
         const key_info_len = if (key_info != null) key_info.?.len else 0;
+
         c.blst_keygen(&sk.value, &ikm[0], ikm.len, key_info_ptr, key_info_len);
         return sk;
     }
 
-    pub fn keyGenV3(ikm: []const u8, key_info: []const u8) BLST_ERROR!SecretKey {
+    pub fn keyGenV3(ikm: []const u8, key_info: ?[]const u8) BLST_ERROR!SecretKey {
         if (ikm.len < 32) {
             return BLST_ERROR.BAD_ENCODING;
         }
 
         var sk = SecretKey.default();
-        c.blst_keygen_v3(&sk.value, &ikm[0], ikm.len, &key_info[0], key_info.len);
+        const key_info_ptr = if (key_info != null) &key_info.?[0] else null;
+        const key_info_len = if (key_info != null) key_info.?.len else 0;
+
+        c.blst_keygen_v3(&sk.value, &ikm[0], ikm.len, key_info_ptr, key_info_len);
         return sk;
     }
 
-    pub fn keyGenV45(ikm: []const u8, salt: []const u8, info: []const u8) BLST_ERROR!SecretKey {
+    pub fn keyGenV45(ikm: []const u8, salt: []const u8, info: ?[]const u8) BLST_ERROR!SecretKey {
         if (ikm.len < 32) {
             return BLST_ERROR.BAD_ENCODING;
         }
 
         var sk = SecretKey.default();
-        c.blst_keygen_v4_5(&sk.value, &ikm[0], ikm.len, &salt[0], salt.len, &info[0], info.len);
+        const info_ptr = if (info != null and info.?.len > 0) &info.?[0] else null;
+        const info_len = if (info != null) info.?.len else 0;
+
+        c.blst_keygen_v4_5(&sk.value, &ikm[0], ikm.len, &salt[0], salt.len, info_ptr, info_len);
         return sk;
     }
 
-    pub fn keyGenV5(ikm: []const u8, salt: []const u8, info: []const u8) BLST_ERROR!SecretKey {
+    pub fn keyGenV5(ikm: []const u8, salt: []const u8, info: ?[]const u8) BLST_ERROR!SecretKey {
         if (ikm.len < 32) {
             return BLST_ERROR.BAD_ENCODING;
         }
 
         var sk = SecretKey.default();
-        c.blst_keygen_v5(&sk.value, &ikm[0], ikm.len, &salt[0], salt.len, &info[0], info.len);
+        const info_ptr = if (info != null and info.?.len > 0) &info.?[0] else null;
+        const info_len = if (info != null) info.?.len else 0;
+
+        c.blst_keygen_v5(&sk.value, &ikm[0], ikm.len, &salt[0], salt.len, info_ptr, info_len);
         return sk;
     }
 
@@ -183,15 +193,17 @@ const PublicKey = struct {
     }
 
     pub fn uncompress(pk_comp: []const u8) BLST_ERROR!PublicKey {
-        if (pk_comp.len == 48 and (pk_comp[0] & 0x80) == 0) {
+        if (pk_comp.len == 48 and (pk_comp[0] & 0x80) != 0) {
             var pk = PublicKey.default();
             const res = c.blst_p1_uncompress(&pk.point, &pk_comp[0]);
             const err = toBlstError(res);
             if (err != null) {
-                return err;
+                return err.?;
             }
             return pk;
         }
+
+        return BLST_ERROR.BAD_ENCODING;
     }
 
     pub fn deserialize(pk_in: []const u8) BLST_ERROR!PublicKey {
@@ -202,7 +214,7 @@ const PublicKey = struct {
             const res = c.blst_p1_deserialize(&pk.point, &pk_in[0]);
             const err = toBlstError(res);
             if (err != null) {
-                return err;
+                return err.?;
             }
             return pk;
         }
@@ -382,7 +394,7 @@ const Signature = struct {
             const res = c.blst_p2_deserialize(&sig.point, &sig_in[0]);
             const err = toBlstError(res);
             if (err != null) {
-                return err;
+                return err.?;
             }
             return sig;
         }
@@ -418,7 +430,7 @@ const AggregateSignature = struct {
         const res = c.blst_p2_in_g2(&self.point);
         const err = toBlstError(res);
         if (err != null) {
-            return err;
+            return err.?;
         }
     }
 
@@ -503,7 +515,53 @@ test "SecretKey" {
     _ = try sk.deriveChildEip2333(0);
 }
 
-// TODO: gen_random_key
+/// this is a simulation of get_random_key() in Rust without having to use chacha20 random
+/// this is not nice but good enough to start with
+/// TODO: use zig equivalent way, could produce different data
+/// equivalent Rust code
+/// ```rust
+/// let seed = [0u8; 32];
+///  let mut rng = ChaCha20Rng::from_seed(seed);
+/// let num_msgs = 10;
+/// (0..num_msgs).for_each(|i| {
+///   let mut msg = [0u8; 32];
+///   rng.fill_bytes(&mut msg);
+/// });
+///
+/// let mut msgs: Vec<Vec<u8>> = vec![vec![]; num_msgs];
+/// (0..num_msgs).for_each(|i| {
+///   let msg_len = (rng.next_u64() & 0x3F) + 1;
+///   msgs[i] = vec![0u8; msg_len as usize];
+///   rng.fill_bytes(&mut msgs[i]);
+/// })
+const RandomKeyFn = *const fn () SecretKey;
+fn getChacha20Rng() RandomKeyFn {
+    const T = struct {
+        threadlocal var i: u8 = 0;
+        fn getRandomKey() SecretKey {
+            const value: [32]u8 = switch (i) {
+                0 => [_]u8{ 118, 184, 224, 173, 160, 241, 61, 144, 64, 93, 106, 229, 83, 134, 189, 40, 189, 210, 25, 184, 160, 141, 237, 26, 168, 54, 239, 204, 139, 119, 13, 199 },
+                1 => [_]u8{ 218, 65, 89, 124, 81, 87, 72, 141, 119, 36, 224, 63, 184, 216, 74, 55, 106, 67, 184, 244, 21, 24, 161, 28, 195, 135, 182, 105, 178, 238, 101, 134 },
+                2 => [_]u8{ 159, 7, 231, 190, 85, 81, 56, 122, 152, 186, 151, 124, 115, 45, 8, 13, 203, 15, 41, 160, 72, 227, 101, 105, 18, 198, 83, 62, 50, 238, 122, 237 },
+                3 => [_]u8{ 41, 183, 33, 118, 156, 230, 78, 67, 213, 113, 51, 176, 116, 216, 57, 213, 49, 237, 31, 40, 81, 10, 251, 69, 172, 225, 10, 31, 75, 121, 77, 111 },
+                4 => [_]u8{ 45, 9, 160, 230, 99, 38, 108, 225, 174, 126, 209, 8, 25, 104, 160, 117, 142, 113, 142, 153, 123, 211, 98, 198, 176, 195, 70, 52, 169, 160, 179, 93 },
+                5 => [_]u8{ 1, 39, 55, 104, 31, 123, 93, 15, 40, 30, 58, 253, 228, 88, 188, 30, 115, 210, 211, 19, 201, 207, 148, 192, 95, 243, 113, 98, 64, 162, 72, 242 },
+                6 => [_]u8{ 19, 32, 160, 88, 215, 179, 86, 107, 213, 32, 218, 170, 62, 210, 191, 10, 197, 184, 177, 32, 251, 133, 39, 115, 195, 99, 151, 52, 180, 92, 145, 164 },
+                7 => [_]u8{ 45, 212, 203, 131, 248, 132, 13, 46, 237, 177, 88, 19, 16, 98, 172, 63, 31, 44, 248, 255, 109, 205, 24, 86, 232, 106, 30, 108, 49, 103, 22, 126 },
+                8 => [_]u8{ 229, 166, 136, 116, 43, 71, 197, 173, 251, 89, 212, 223, 118, 253, 29, 177, 229, 30, 224, 59, 28, 169, 248, 42, 202, 23, 62, 219, 139, 114, 147, 71 },
+                9 => [_]u8{ 78, 190, 152, 15, 144, 77, 16, 201, 22, 68, 43, 71, 131, 160, 233, 132, 134, 12, 182, 201, 87, 179, 156, 56, 237, 143, 81, 207, 250, 166, 138, 77 },
+                else => @panic("getRadomKey() is not implemented for big number"),
+            };
+            i += 1;
+            const sk = SecretKey.keyGen(value[0..], null) catch {
+                @panic("SecretKey.keyGen() failed\n");
+            };
+            return sk;
+        }
+    };
+
+    return T.getRandomKey;
+}
 
 test "test_sign_n_verify" {
     const ikm: [32]u8 = [_]u8{
@@ -514,7 +572,6 @@ test "test_sign_n_verify" {
     };
     const sk = try SecretKey.keyGen(ikm[0..], null);
     const pk = sk.skToPk();
-    std.debug.print("pk: {any}\n", .{pk.serialize()});
 
     const dst = "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
     const msg = "hello foo";
@@ -523,4 +580,73 @@ test "test_sign_n_verify" {
 
     // aug is null
     try sig.verify(true, msg[0..], dst[0..], null, &pk, true);
+}
+
+test "test_aggregate" {
+    const num_msgs = 10;
+    const dst = "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
+
+    const getRandomKey = getChacha20Rng();
+    var sks = [_]SecretKey{SecretKey.default()} ** num_msgs;
+    for (0..num_msgs) |i| {
+        sks[i] = getRandomKey();
+    }
+
+    var pks: [num_msgs]PublicKey = undefined;
+    const pksSlice = pks[0..];
+    for (0..num_msgs) |i| {
+        pksSlice[i] = sks[i].skToPk();
+    }
+
+    var pks_ptr: [num_msgs]*PublicKey = undefined;
+    var pks_ptr_rev: [num_msgs]*PublicKey = undefined;
+    for (pksSlice, 0..num_msgs) |*pk_ptr, i| {
+        pks_ptr[i] = pk_ptr;
+        pks_ptr_rev[num_msgs - i - 1] = pk_ptr;
+    }
+
+    const pk_comp = pksSlice[0].compress();
+    _ = try PublicKey.uncompress(pk_comp[0..]);
+
+    var msgs: [num_msgs][]u8 = undefined;
+    const msg_lens: [num_msgs]u64 = comptime .{ 33, 34, 39, 22, 43, 1, 24, 60, 2, 41 };
+
+    inline for (0..num_msgs) |i| {
+        var msg = [_]u8{0} ** msg_lens[i];
+        msgs[i] = msg[0..];
+        std.crypto.random.bytes(msgs[i]);
+    }
+
+    var sigs: [num_msgs]Signature = undefined;
+    for (0..num_msgs) |i| {
+        sigs[i] = sks[i].sign(msgs[i], dst, null);
+    }
+
+    for (0..num_msgs) |i| {
+        try sigs[i].verify(true, msgs[i], dst, null, pks_ptr[i], true);
+    }
+
+    // Swap message/public key pairs to create bad signature
+    for (0..num_msgs) |i| {
+        if (sigs[i].verify(true, msgs[num_msgs - i - 1], dst, null, pks_ptr_rev[i], true)) {
+            try std.testing.expect(false);
+        } else |err| {
+            try std.testing.expectEqual(err, BLST_ERROR.VERIFY_FAIL);
+        }
+    }
+
+    var sig_ptrs: [num_msgs]*Signature = undefined;
+    for (sigs[0..], 0..num_msgs) |*sig_ptr, i| {
+        sig_ptrs[i] = sig_ptr;
+    }
+    const agg = try AggregateSignature.aggregate(sig_ptrs[0..], true);
+    _ = agg.toSignature();
+    // TODO aggregate_verify
+    // let mut result = agg_sig
+    //   .aggregate_verify(false, &msgs_refs, dst, &pks_refs, false);
+    // assert_eq!(result, BLST_ERROR::BLST_SUCCESS);
+    // // Swap message/public key pairs to create bad signature
+    // result = agg_sig
+    //     .aggregate_verify(false, &msgs_refs, dst, &pks_rev, false);
+    // assert_ne!(result, BLST_ERROR::BLST_SUCCESS);
 }
