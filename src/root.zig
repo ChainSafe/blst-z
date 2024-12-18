78dc1b1410bfa5d69becdadc35866b50994698c2
Tuyen Nguyen <twoeths@users.noreply.github.com>
2024-12-18 09:32:28 +0700

    feat: move tests to sig_variable.zig to make it reusable later

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const testing = std.testing;
-pub const MinPk = @import("./sig_variant_min_pk.zig").MinPk;
+pub const MinPk = @import("./sig_variant_min_pk.zig").SigVariant;
 
 test {
     testing.refAllDecls(@This());
