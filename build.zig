14d7a39d98cefaf4132f8db303c693b1eb31f260
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-03-17 15:20:27 +0700

    chore: add TODOs

--- build.zig
+++ build.zig
@@ -121,7 +121,8 @@ pub fn build(b: *std.Build) !void {
 
 /// instead of treating blst as a dependency lib, build and link it, we add its resource to our libs
 /// and zig will handle a mixture of C, assembly and Zig code
-///  reference to https://github.com/supranational/blst/blob/v0.3.13/bindings/rust/build.rs
+/// reference to https://github.com/supranational/blst/blob/v0.3.13/bindings/rust/build.rs
+/// TODO: port all missing flows from the Rust build script
 fn withBlst(b: *std.Build, blst_z_lib: *Compile, target: ResolvedTarget, is_shared_lib: bool, portable: bool, force_adx: bool) !void {
     // add later, once we have cflags
     const arch = target.result.cpu.arch;
