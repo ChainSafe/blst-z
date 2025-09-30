58cc49e5b2efc70f6733dc0674b4020b453f8b48
bing <spiralladder@fastmail.com>
2025-09-30 16:31:08 +0800

    fix signature.zig

--- src/root.zig
+++ src/root.zig
@@ -4,7 +4,7 @@ const testing = std.testing;
 pub const Pairing = @import("Pairing.zig");
 pub const SecretKey = @import("SecretKey.zig");
 pub const PublicKey = @import("PublicKey.zig");
-pub const Signature = @import("signature.zig");
+pub const Signature = @import("Signature.zig");
 pub const AggregatePublicKey = @import("AggregatePublicKey.zig");
 pub const AggregateSignature = @import("AggregateSignature.zig");
 
