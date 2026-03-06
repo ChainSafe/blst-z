f752638bb73923517179de9d1b65090c6f28c87c
Jeff Chung <sh1001309@gmail.com>
2026-03-06 15:21:25 +0800

    feat: update fuzz tests to include additional imports for signature and aggregate functionalities

--- test/fuzz/main.zig
+++ test/fuzz/main.zig
@@ -1,3 +1,6 @@
 comptime {
-    _ = @import("publicKey.zig");
+    _ = @import("public_key.zig");
+    _ = @import("signature.zig");
+    _ = @import("aggregate_pk.zig");
+    _ = @import("aggregate_sig.zig");
 }
